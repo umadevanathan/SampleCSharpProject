b8085edcd362f08dd12d025ece68f6d1d681c109
umait39 <65897398+umait39@users.noreply.github.com>
2020-12-17 19:35:44 +0530

    Add files via upload

---
+++ SomeConsoleApplication/Program2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeConsoleApplication
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+        }
+    }
+}
